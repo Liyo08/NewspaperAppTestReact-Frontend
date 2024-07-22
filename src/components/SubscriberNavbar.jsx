4705794660376fbe607656a051d40c1ac63f9cb6
Liyo08 <liyotom04@gmail.com>
2024-07-22 20:27:20 +0530

    S.P.A. implemented

--- src/components/SubscriberNavbar.jsx
+++ src/components/SubscriberNavbar.jsx
@@ -1,27 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const SubscriberNavbar = () => {
   return (
     <div>
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Newspaper-App</a>
+    <Link class="navbar-brand" to="#">Newspaper-App</Link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">Add</a>
+          <Link class="nav-link active" aria-current="page" to="/">Add</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/search">Search</a>
+          <Link class="nav-link" to="/search">Search</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/delete">Delete</a>
+          <Link class="nav-link" to="/delete">Delete</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/view">View All</a>
+          <Link class="nav-link" to="/view">View All</Link>
         </li>
        
       </ul>
