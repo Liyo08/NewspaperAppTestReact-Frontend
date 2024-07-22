990414e4dcabbf17f019c87bbb0cf3e2f64ac6e6
Liyo08 <liyotom04@gmail.com>
2024-07-22 19:51:23 +0530

    SearchSubscribers added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import AddSubscribersPage from './components/AddSubscribersPage';
+import SearchSubscribers from './components/SearchSubscribers';
 
 function App() {
   return (
     <div>
      <AddSubscribersPage/>
+     <SearchSubscribers/>
+     
     </div>
   );
 }
