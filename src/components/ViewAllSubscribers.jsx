15fa4033cd7d193226a79449d1d382bad082ad63
Liyo08 <liyotom04@gmail.com>
2024-07-22 20:58:22 +0530

    Table details added

--- src/components/ViewAllSubscribers.jsx
+++ src/components/ViewAllSubscribers.jsx
@@ -9,30 +9,131 @@ const ViewAllSubscribers = () => {
 <table class="table">
   <thead>
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">Name</th>
+      <th scope="col">Phone Number</th>
+      <th scope="col">Pin Code</th>
+      <th scope="col">District</th>
+      <th scope="col">Village Office</th>
+      <th scope="col">Email ID</th>
+      <th scope="col">Address</th>
     </tr>
   </thead>
   <tbody>
     <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
+      <th scope="row">Mark Ruth</th>
+      <td>9365683935</td>
+      <td>682647</td>
+      <td>Ernakulam</td>
+      <td>Pulickamali</td>
+      <td>ruth@gmail.com</td>
+      <td>Parekkavu(H),Pulickamali,Arakkunam</td>
     </tr>
     <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
+      <th scope="row">Jacob Mathew</th>
+      <td>9123456654</td>
+      <td>682453</td>
+      <td>Ernakulam</td>
+      <td>Puthenkurish</td>
+      <td>jacob@gmail.com</td>
+      <td>Moolamatathil(H),Puthenkurish,Thiruvankulam</td>
     </tr>
     <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
+      <th scope="row">Larry Savior</th>
+      <td>9876678542</td>
+      <td>682341</td>
+      <td>Ernakulam</td>
+      <td>Muvattupuzha</td>
+      <td>larry23@gmail.com</td>
+      <td>Palayil(H),Kodiyanad,Muvattupuzha</td>
     </tr>
+
+
+    <tr>
+      <th scope="row">Kuriakose Mathan</th>
+      <td>9342678543</td>
+      <td>682564</td>
+      <td>Ernakulam</td>
+      <td>Piravom</td>
+      <td>kurian87@gmail.com</td>
+      <td>Kaiparambatt(H),Edayar,Piravom</td>
+    </tr>
+
+
+    <tr>
+      <th scope="row">Alex Joseph</th>
+      <td>9456342654</td>
+      <td>682397</td>
+      <td>Ernakulam</td>
+      <td>Maamala</td>
+      <td>alexjoseph45@gmail.com</td>
+      <td>Palamattathil(H),Mamalakavala,Mamala</td>
+    </tr>
+
+
+
+    <tr>
+      <th scope="row">Thomas Paul</th>
+      <td>9098876109</td>
+      <td>682776</td>
+      <td>Ernakulam</td>
+      <td>Vettickal</td>
+      <td>thomasp09@gmail.com</td>
+      <td>Thenkunnam(H),Thiruvaniyoor,Vettickal</td>
+    </tr>
+
+
+
+    <tr>
+      <th scope="row">Joel Kuriachan</th>
+      <td>9987789605</td>
+      <td>682223</td>
+      <td>Ernakulam</td>
+      <td>Thalacode</td>
+      <td>joel2004@gmail.com</td>
+      <td>Koorinayil(H),Thuppumpady,Thalacode</td>
+    </tr>
+
+
+
+
+    <tr>
+      <th scope="row">Alan J</th>
+      <td>9223344332</td>
+      <td>682771</td>
+      <td>Ernakulam</td>
+      <td>Kolencherry</td>
+      <td>alanj878@gmail.com</td>
+      <td>Ettanparayil(H),Choondi,Kolencherry</td>
+    </tr>
+
+
+
+
+    <tr>
+      <th scope="row">Sanal Varma</th>
+      <td>9556423123</td>
+      <td>68342</td>
+      <td>Ernakulam</td>
+      <td>Vattukkunu</td>
+      <td>sanal007@gmail.com</td>
+      <td>Valakkamatathu(H),Eruveli,Mulanthuruthy</td>
+    </tr>
+
+
+
+
+    <tr>
+      <th scope="row">Jess Mon</th>
+      <td>9000900876</td>
+      <td>683453</td>
+      <td>Alapuzha</td>
+      <td>Kalavoor</td>
+      <td>jessmon333@gmail.com</td>
+      <td>Nadumparaickal(H),Kalanidhilayam,Kalavoor</td>
+    </tr>
+
+
+   
   </tbody>
 </table>
 
