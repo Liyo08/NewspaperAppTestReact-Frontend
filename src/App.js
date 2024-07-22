cc095ae2d73c39497884d1262911179f1a159c5f
Liyo08 <liyotom04@gmail.com>
2024-07-22 19:57:00 +0530

    DeleteSubscriber added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import AddSubscribersPage from './components/AddSubscribersPage';
 import SearchSubscribers from './components/SearchSubscribers';
+import DeleteSubscriber from './components/DeleteSubscriber';
 
 function App() {
   return (
     <div>
      <AddSubscribersPage/>
      <SearchSubscribers/>
+     <DeleteSubscriber/>
      
     </div>
   );
